76c070cb3f021b093e91fe3f624555c73b1baed3
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-04-25 15:57:00 -0500

    -

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,18 +1,41 @@
 const router = require('express').Router();
 const addPage = require('../views/addPage');
+const {Page} = require("../models");
 
-router.get('/', (req, res) => {
-  res.send('we made it to GET WIKI');
-});
-
-router.post('/', (req, res) => {
-  res.send('we got to POST WIKI');
-});
+console.log('page: ', Page)
 
 router.get('/add', (req, res) => {
   //res.send(addPage());
   res.send(addPage());
 })
+
+router.get('/', (req, res) => {
+  res.redirect('/');
+});
+
+router.post('/', async (req, res, next) => {
+  // res.json(req.body);
+  console.log(req.body.title);
+  
+
+  // const page = new Page({
+  //   title: req.body.title,
+  //   content: req.body.content
+  // })
+
+  // console.log(page);
+
+  try {
+
+    await model.Page.create({title: req.body.title,
+    content: req.body.content})
+    // await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
+});
+
+
+
 
 
 
