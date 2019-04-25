0931e867ddee4f2ef9d91f0799fadf7c660768a7
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-04-25 16:57:43 -0500

    completed through sending the page

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
+const {Page} = require("../models");
 
 
-
-
+console.log('Page in user.js is: ', Page)
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,42 +1,42 @@
 const router = require('express').Router();
-const addPage = require('../views/addPage');
 const {Page} = require("../models");
+const wikipage = require('../views/wikipage')
 
-console.log('page: ', Page)
 
+const addPage = require('../views/addPage');
 router.get('/add', (req, res) => {
-  //res.send(addPage());
   res.send(addPage());
 })
+
+
+router.get('/:slug',async  (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+
+    res.send(wikipage(req.body));
+
+  } catch (error) {
+    next(error) }
+
+  });
 
 router.get('/', (req, res) => {
   res.redirect('/');
 });
 
 router.post('/', async (req, res, next) => {
-  // res.json(req.body);
-  console.log(req.body.title);
-  
-
-  // const page = new Page({
-  //   title: req.body.title,
-  //   content: req.body.content
-  // })
-
-  // console.log(page);
-
   try {
-
-    await model.Page.create({title: req.body.title,
+    await Page.create({title: req.body.title,
     content: req.body.content})
-    // await page.save();
-    res.redirect('/');
+    console.log(req.body)
+    res.redirect(`/wiki/:${res.body.slug}`);
   } catch (error) { next(error) }
+  
 });
-
-
-
-
 
 
 module.exports = router;
